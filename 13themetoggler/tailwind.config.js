981d01f03be7873b694026003bb41284060cc9dd
ankitsingh49578 <ankitsingh49578@gmail.com>
2024-11-14 11:26:27 +0530

    theme changer

---
+++ 13themetoggler/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",        // this should be added to enable dark theme
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
